386c2542c0474162620f0d6176fc29941a9cc16b
Tom Cumming <mail@tomcumming.co.uk>
2020-09-25 16:16:07 +0100

    Add negative test for duals

--- test/dual.test.ts
+++ test/dual.test.ts
@@ -4,26 +4,26 @@ import {Algebra, Term} from '../algebra.ts';
 
 // The following is a list of duals from the PGA4CS paper
 const exampleDuals: [Term[], Term[]][] = [
-    [[1], [1, [0], [1], [2], [3]]],
+    [[1], [[0], [1], [2], [3]]],
 
-    [[1, [0], [1], [2], [3]], [1]],
+    [[[0], [1], [2], [3]], [1]],
 
-    [[1, [0]], [1, [1], [2], [3]]],
-    [[1, [1]], [1, [0], [3], [2]]],
-    [[1, [2]], [1, [0], [1], [3]]],
-    [[1, [3]], [1, [0], [2], [1]]],
+    [[[0]], [[1], [2], [3]]],
+    [[[1]], [[0], [3], [2]]],
+    [[[2]], [[0], [1], [3]]],
+    [[[3]], [[0], [2], [1]]],
 
-    [[1, [1], [2], [3]], [-1, [0]]],
-    [[1, [0], [3], [2]], [-1, [1]]],
-    [[1, [0], [1], [3]], [-1, [2]]],
-    [[1, [0], [2], [1]], [-1, [3]]],
+    [[[1], [2], [3]], [-1, [0]]],
+    [[[0], [3], [2]], [-1, [1]]],
+    [[[0], [1], [3]], [-1, [2]]],
+    [[[0], [2], [1]], [-1, [3]]],
 
-    [[1, [0], [1]], [1, [2], [3]]],
-    [[1, [0], [2]], [1, [3], [1]]],
-    [[1, [0], [3]], [1, [1], [2]]],
-    [[1, [2], [3]], [1, [0], [1]]],
-    [[1, [3], [1]], [1, [0], [2]]],
-    [[1, [1], [2]], [1, [0], [3]]],
+    [[[0], [1]], [[2], [3]]],
+    [[[0], [2]], [[3], [1]]],
+    [[[0], [3]], [[1], [2]]],
+    [[[2], [3]], [[0], [1]]],
+    [[[3], [1]], [[0], [2]]],
+    [[[1], [2]], [[0], [3]]],
 ];
 
 const pga3d = new Algebra(3, 0 ,1);
@@ -32,6 +32,15 @@ Deno.test("PGA3D duals match PGA4CS examples", () => {
     for(const [left, right] of exampleDuals) {
         const leftSimplified = pga3d.simplify(left);
         const rightSimplified = pga3d.simplify(right);
+
+        assertEquals(pga3d.dual(leftSimplified), rightSimplified);
+    }
+});
+
+Deno.test("PGA3D duals match PGA4CS examples when negative", () => {
+    for(const [left, right] of exampleDuals) {
+        const leftSimplified = pga3d.simplify([-1, ...left]);
+        const rightSimplified = pga3d.simplify([-1, ...right]);
 
         assertEquals(pga3d.dual(leftSimplified), rightSimplified);
     }
